21105b30cd91a5ed5b8cf9f14a4d88316ccdbf06
Ivan Bielozorov <vanya2143@gmail.com>
2021-03-25 14:04:12 +0200

    Add custom CreateModelMixin

---
+++ shortener/mixins.py
@@ -0,0 +1,30 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.settings import api_settings
+
+
+class CustomCreateModelMixin:
+    """
+    Create a model instance.
+    """
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        data = {
+            "short_url": serializer.data.get('short_url')
+        }
+
+        return Response(data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def get_success_headers(self, data):
+        try:
+            return {'Location': str(data[api_settings.URL_FIELD_NAME])}
+        except (TypeError, KeyError):
+            return {}
