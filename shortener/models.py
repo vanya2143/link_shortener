f0b5b913b4e1cbb02e3ffea48c8bebba7d5e9d45
vanya2143 <vanya2143@gmail.com>
2021-04-04 18:59:53 +0300

    Added get_site_name function.

--- shortener/models.py
+++ shortener/models.py
@@ -1,7 +1,14 @@
 from hashlib import sha256
 from time import time
+import re
 
 from django.db import models
+
+pattern = re.compile(r'http?.:(?:\/\/127\.0{0,3}\.0{0,3}.0{0,2}1:\d)?(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)')
+
+
+def get_site_name(url):
+    return pattern.match(url).group()
 
 
 class Link(models.Model):
@@ -14,7 +21,7 @@ class Link(models.Model):
         return sha256(url).hexdigest()[:8]
 
     def __str__(self):
-        return self.title
+        return f'{self.hash} to {get_site_name(self.url)}'
 
     def save(self, *args, **kwargs):
         self.hash = self.create_source_link()
